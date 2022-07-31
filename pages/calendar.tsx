92ac2921ee95a18c416bd116dc6edbb20a5afd47
Gracias <gracias.claude@outlook.com>
2022-07-31 16:32:58 -0600

    gc/feature-calendar Spacing/UI - fix spacing issue with headers - change headline colors to text-purple-400 - change feedback color to text-bg-900

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { CheckIcon, ThumbUpIcon, UserIcon } from "@heroicons/react/solid";
+import {
+  CheckIcon,
+  CogIcon,
+  ThumbUpIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
+//TODO: Create a startDay stateObject
 
 const Calendar = (props) => {
   const daysOfWeek = [
@@ -14,118 +20,114 @@ const Calendar = (props) => {
   const timeline = [{
     id: 1,
     content: "Create Events",
-    target: "Front End Developer",
+    // target: "Front End Developer",
     href: "#",
-    date: "Sep 20",
+    // date: "Sep 20",
     datetime: "2020-09-20",
-    icon: UserIcon,
+    icon: CogIcon,
     iconBackground: "bg-gray-400",
   }, {
     id: 2,
-    content: "Advanced to phone screening by",
-    target: "Bethany Blake",
+    content: "Delete Events",
+    // target: "Bethany Blake",
     href: "#",
-    date: "Sep 22",
+    // date: "Sep 22",
     datetime: "2020-09-22",
-    icon: ThumbUpIcon,
-    iconBackground: "bg-blue-500",
+    icon: CogIcon,
+    iconBackground: "bg-gray-400",
   }, {
     id: 3,
-    content: "Completed phone screening with",
-    target: "Martha Gardner",
+    content: "Light/Dark Mode",
+    // target: "Bethany Blake",
     href: "#",
-    date: "Sep 28",
-    datetime: "2020-09-28",
-    icon: CheckIcon,
-    iconBackground: "bg-green-500",
+    // date: "Sep 22",
+    datetime: "2020-09-22",
+    icon: CogIcon,
+    iconBackground: "bg-purple-400",
   }, {
     id: 4,
-    content: "Advanced to interview by",
-    target: "Bethany Blake",
+    content: "Edit Events",
+    // target: "Bethany Blake",
     href: "#",
-    date: "Sep 30",
-    datetime: "2020-09-30",
-    icon: ThumbUpIcon,
-    iconBackground: "bg-blue-500",
-  }, {
-    id: 5,
-    content: "Completed interview with",
-    target: "Katherine Snyder",
-    href: "#",
-    date: "Oct 4",
-    datetime: "2020-10-04",
-    icon: CheckIcon,
-    iconBackground: "bg-green-500",
+    // date: "Sep 22",
+    datetime: "2020-09-22",
+    icon: CogIcon,
+    iconBackground: "bg-gray-400",
   }];
+
   const handleCalendarLayout = () => {
     const rows = Math.ceil(props.days / 7);
     let startDay = 1;
     return (
       <div className="flex h-full font-mono">
-        <div className="w-1/2 flow-root grid justify-center items-end">
-          <ul role="list" className="-mb-8">
-            {timeline.map((event, eventIdx) => (
-              <li key={event.id}>
-                <div className="relative pb-8">
-                  {eventIdx !== timeline.length - 1
-                    ? (
-                      <span
-                        className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                        aria-hidden="true"
-                      />
-                    )
-                    : null}
-                  <div className="relative flex space-x-3">
-                    <div>
-                      <span
-                        className={classNames(
-                          event.iconBackground,
-                          "h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white",
-                        )}
-                      >
-                        <event.icon
-                          className="h-5 w-5 text-white"
+        <div className="w-1/2 flow-root grid justify-center items-center">
+          <div>
+            <h1 className="text-xl mb-4 bold grid justify-center">
+              Features
+            </h1>
+            <ul role="list" className="-mb-8">
+              {timeline.map((event, eventIdx) => (
+                <li key={event.id}>
+                  <div className="relative pb-8">
+                    {eventIdx !== timeline.length - 1
+                      ? (
+                        <span
+                          className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
                           aria-hidden="true"
                         />
-                      </span>
-                    </div>
-                    <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                      )
+                      : null}
+                    <div className="relative flex space-x-3">
                       <div>
-                        <p className="text-sm text-gray-500">
-                          {event.content}{" "}
-                          <a
-                            href={event.href}
-                            className="font-medium text-gray-900"
-                          >
-                            {event.target}
-                          </a>
-                        </p>
+                        <span
+                          className={classNames(
+                            event.iconBackground,
+                            "h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white",
+                          )}
+                        >
+                          <event.icon
+                            className="h-5 w-5 text-white"
+                            aria-hidden="true"
+                          />
+                        </span>
                       </div>
-                      <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                        <time dateTime={event.datetime}>{event.date}</time>
+                      <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                        <div>
+                          <p className="text-sm text-gray-500">
+                            {event.content} {
+                              // <a
+                              //   href={event.href}
+                              //   className="font-medium text-gray-900"
+                              // >
+                              //   {event.target}
+                              // </a>
+                            }
+                          </p>
+                        </div>
+                        <div className="text-right text-sm whitespace-nowrap text-gray-500">
+                          <time dateTime={event.datetime}>{event.date}</time>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className=" text-md p-2 w-1/2 grid justify-center items-end">
-          <h1 className="grid justify-center text-lg text-purple-400">
+        <div className=" text-sm w-2/3 mt-16 grid justify-center ">
+          <h1 className="grid justify-center mb-2  text-xl ">
             {props.month} {props.year}
           </h1>
-          <div className="grid grid-cols-7 gap-4 ">
+          <div className="grid grid-cols-7">
             {daysOfWeek.map((day) => {
               return <h1 className="flex justify-center">{day[0]}</h1>;
             })}
           </div>
           {[...Array(rows)].map((e, i) => {
             return (
-              <div
-                className={`grid grid-rows-${rows} text-center gap-4`}
-              >
-                <div className="grid grid-cols-7 gap-4 box-content border-2 divide-x ">
+              <div className={`grid grid-rows-${rows} text-center `}>
+                <div className="grid grid-cols-7 box-content border-2 divide-x ">
                   {daysOfWeek.map((day) => {
                     return (
                       <div
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,9 @@ const Home = () => {
       <div className="text-white h-1/2 p-12 bg-gray-900">
         <div className="flex">
           <div className="flex-1 flex justify-between">
-            <h1 className="text-2xl font-light cursor-pointer">Calendar</h1>
+            <h1 className="text-2xl font-light cursor-pointer text-purple-400">
+              Calendar
+            </h1>
           </div>
           <div>
             <div className="flex text-lg">
@@ -42,7 +44,7 @@ const Home = () => {
               <i className="fa fa-instagram mx-3 mt-1"></i>
               <i className="fa fa-youtube mx-3 mt-1"></i>
               <i className="fa fa-facebook mx-3 mt-1"></i>
-              <h1 className="bg-white text-purple-300 px-2 py-1 -mt-1 mx-3 text-lg cursor-pointer">
+              <h1 className=" rounded-md bg-white text-gray-900 px-2 py-1 -mt-1 mx-3 text-lg cursor-pointer">
                 Feedback
               </h1>
             </div>
