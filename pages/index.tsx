4df3e2b9eaa6ff67521ae56c619dcf853520a8c1
Gracias <gracias.claude@outlook.com>
2022-08-02 09:15:47 -0600

    gc/debt-componetize - componentize header - Add HeaderProps type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "./components/header";
 import React, { useEffect, useState } from "react";
 import {
   getDaysInMonth,
@@ -27,29 +28,7 @@ const Home = () => {
   return (
     <div className="bg-white h-screen">
       <div className="text-white h-1/2 p-12 bg-gray-900">
-        <div className="flex">
-          <div className="flex-1 flex justify-between">
-            <h1 className="text-2xl font-light cursor-pointer text-purple-400">
-              Calendar
-            </h1>
-          </div>
-          <div>
-            <div className="flex text-lg">
-              <h1 className="mx-2 border-b-2 border-transparent hover:border-white cursor-pointer">
-                About
-              </h1>
-              <h1 className="mx-2 border-b-2 border-transparent hover:border-white cursor-pointer">
-                Contact
-              </h1>
-              <i className="fa fa-instagram mx-3 mt-1"></i>
-              <i className="fa fa-youtube mx-3 mt-1"></i>
-              <i className="fa fa-facebook mx-3 mt-1"></i>
-              <h1 className=" rounded-md bg-white text-gray-900 px-2 py-1 -mt-1 mx-3 text-lg cursor-pointer">
-                Feedback
-              </h1>
-            </div>
-          </div>
-        </div>
+        <Header headers={["About", "Contact"]} />
         <div className="p-24 flex justify-center">
           <h1 className="text-3xl font-serif">
             This is a project idea from the{" "}
